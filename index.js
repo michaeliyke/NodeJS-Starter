66dbb1005fc6f1ecc375a07c9247abe94f490891
Michael C. Iyke <ymichaelc@gmail.com>
2020-09-18 00:33:36 +0100

    Serving HTML files

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-  console.log(request.headers);
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/html");
-  response.end("<html><body><h1>Hwllo World</h1></body></html>");
+  console.log(request.headers, request.url, request.method);
+  if (request.method == "GET") {
+    const fileUrl = request.url == "/" ? "/index.html" : request.url;
+    const filePath = path.resolve(`./plublic${fileUrl}`);
+    const file_ext = path.extname(filePath);
+    if (file_ext == ".html") {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          response.statusCode = 404;
+          response.setHeader("Content-Type", "text/html");
+          response.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+          return
+        }
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(response);
+      });
+    } else {
+      response.statusCode = 404;
+          response.setHeader("Content-Type", "text/html");
+          response.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+          return
+    }
+  } else {
+    response.statusCode = 404;
+          response.setHeader("Content-Type", "text/html");
+          response.end(`<html><body><h1>Error 404: ${request.method} not supported</h1></body></html>`);
+          return
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Sever running at http://${hostname}:${port}`)
 });
+
+
+// 
