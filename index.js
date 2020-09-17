ba5942ddb1b162392e9e82bfa2b2400f824c8644
Michael C. Iyke <ymichaelc@gmail.com>
2020-09-17 13:53:16 +0100

    Using Callbacks and Error Handling

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const rect = require("./rectangle");
 
-
-function solveRect(l, b) {
-  if ((l == +l && l > 0) && (b == +b && b > 0)) {
-    console.log("Area: ", rect.area(l, b));
-    console.log("Perimeter: ", rect.perimeter(l, b));
-    return console.log("\n");
-  }
-  // 
-  console.log("Both dimensions must be positive non-zero numberer");
-}
+function solveRect (l, b) {
+  rect(l, b, (error, R) => {
+    if (error) {
+      console.log(error);
+      return 
+    }
+    console.log("Area: ", R.area());
+    console.log("Perimeter: ", R.perimeter(), "\n");
+  });
+};
 
 solveRect(2, 4);
 solveRect(3, 5);
--- rectangle.js
+++ rectangle.js
@@ -1,5 +1,14 @@
-
-exports.perimeter = (x, y) => (2 * (x + y));
-
-exports.area = (x, y) => (x * y);
-
+module.exports = (x, y, cb) => {
+  if ((x == +x && x > 0) && (y == +y && y > 0)) {
+    setTimeout(() => {
+      cb(null, {
+        perimeter: () => 2 * (x + y),
+        area: () => x * y
+      });
+    }, 2000);
+  } else {
+    setTimeout(() => {
+      cb(new Error("Both dimensions must be positive non-zero numberer"), null);
+    }, 2000);
+  }
+};
