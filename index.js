d3969aef5b516553e2ac05cd8413f416e7e8f6d0
Michael C. Iyke <ymichaelc@gmail.com>
2020-09-18 21:26:12 +0100

    More renaming

--- index.js
+++ index.js
@@ -1,57 +1,44 @@
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const fileupload = require("express-fileupload");
-
-const app = express();
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const hostname = "localhost";
 const port = 3000;
 
-app.use('/form', express.static(__dirname + '/public/index.html'));
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(fileupload());
-const knex = require("knex")({
-  client: "sqlite3",
-  useNullAsDefault: true,
-  connection: {
-    filename: "./public/sqlite3/Node-starter.sqlite"
-  }
-});
-
-
-
-
-app.get("/", async (request, response) => {
-  response.send('/public/index.html');
-});
-
-app.post("/public", async (request, response) => {
-  if (request.files || Object.keys(request.files) == 0) {
-    response.status(400).send("Upload failed!");
-    return
-  }
-  const photograph = request.files.photograph;
-  await knex.insert({
-    name: photograph.name, 
-    image: photograph.data //The actual binary buffer
-  }).into("images");
-  photograph.mv("/public/uploads/", async (error) => {
-    return error ? response.status(500).send(error) : response.send("Upload successful!");
-  });
-  console.log(photograph);
-  response.sendStatus(200);
-});
-
-app.get("/image/:id", async (request, response) => {
-  const id = request.params.id;
-  const image = await knex("images").where({id: id}).first();
-  if (image) {
-    response.end(image.image);
+const server = http.createServer((request, response) => {
+  console.log(request.headers, request.url, request.method);
+  if (request.method == "GET") {
+    const fileUrl = request.url == "/" ? "/index.html" : request.url;
+    const filePath = path.resolve(`./plublic${fileUrl}`);
+    const file_ext = path.extname(filePath);
+    if (file_ext == ".html") {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          response.statusCode = 404;
+          response.setHeader("Content-Type", "text/html");
+          response.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+          return
+        }
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/html");
+        fs.createReadStream(filePath).pipe(response);
+      });
+    } else {
+      response.statusCode = 404;
+          response.setHeader("Content-Type", "text/html");
+          response.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+          return
+    }
   } else {
-    response.end("Source not found: ")
+    response.statusCode = 404;
+          response.setHeader("Content-Type", "text/html");
+          response.end(`<html><body><h1>Error 404: ${request.method} not supported</h1></body></html>`);
+          return
   }
 });
 
-app.listen(port, 
-  () => console.log(`Server listen on http://127.0.0.1:${port}`));
+server.listen(port, hostname, () => {
+  console.log(`Sever running at http://${hostname}:${port}`)
+});
+
+
+// 
