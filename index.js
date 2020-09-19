feb252473171337c5c5d2055560a46cd003f5641
Michael C. Iyke <ymichaelc@gmail.com>
2020-09-19 10:57:16 +0100

    writing to both db and file system achieved

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const express = require("express");
 const upload = require("express-fileupload");
 
 const app = express();
+const knex = require("knex")({
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./sqlite3/Node-starter.sqlite"
+  }
+});
 
 app.use(upload());
 
@@ -11,21 +18,31 @@ app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/index.html`);
 });
 
-app.post("/", (request, response) => {
+// POST
+app.post("/", async (request, response) => {
   const files = request.files;
 
   if (!files) {
     response.status(404).send("<h1>File not found</h1>");
     return
   }
+
   console.log(`Upload of ${files.photograph.name} received!`);
-  files.photograph.mv(`./uploads/${files.photograph.name}`, (error) => {
+
+  await knex.insert({
+    name: files.photograph.name,
+    image: files.photograph.data //The actual binary buffer
+  }).into("images");
+
+  files.photograph.mv(`./uploads/${files.photograph.name}`, async (error) => {
     if (error) {
-      response.status(500).send("<h1>Internal server error.</h1>");
+      console.log("Upload failed: Internal server error.");
+      response.status(500).send("<h1>Upload failed: Internal server error</h1>");
       return
+    } else {
+      console.log("File successfully ploaded!");
+      response.status(200).send("<h1>File successfully ploaded!</h1>");
     }
-    console.log("File successfully ploaded!");
-    response.status(200).send("<h1>File successfully ploaded!</h1>");
   });
 });
 
